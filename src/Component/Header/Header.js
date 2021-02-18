430c5db779bfc760975ab89635c7de36bbd199ae
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-18 15:15:40 -0800

    Create header and NavBar layout

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Flexbox by example</h1>
+      <p>Everything you need to know to learn flexbox.</p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Component/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ui>
+        <li>Start Here</li>
+        <li>Examples</li>
+        <li>Patterns</li>
+        <li>Video tutorial</li>
+        <li>Resources</li>
+      </ui>
+    </div>
+  );
+};
+
+export default NavBar;
